2ad9e0fee3a0449f18ad9316e6aae169998bbe7f
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-07 12:40:17 -0300

    feat: Creating table and started to fill the data in it

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 import * as C from "./App.styles";
-import {Item} from './types/Item';
-import {Category} from './types/Category';
-import {categories} from './data/categories';
-import {items} from './data/items';
-import {getCurrentMonth,filterByMonth} from './helpers/dateFilter';
+import { Item } from "./types/Item";
+
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import { getCurrentMonth, filterByMonth } from "./helpers/dateFilter";
+import { TableArea } from "./components/tableArea";
 
 const App = () => {
-  const [list,setList] = useState(items);
-  const [filteredList,setFilteredList] = useState<Item[]>([]);
-  const [currentMonth,setCurrentMonth] = useState(getCurrentMonth());
-    
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
   useEffect(() => {
-    setFilteredList(filterByMonth(list,currentMonth));
-  } , [list,currentMonth]);
+    setFilteredList(filterByMonth(list, currentMonth));
+  }, [list, currentMonth]);
 
   return (
     <C.Container>
@@ -25,9 +27,7 @@ const App = () => {
 
         {/* Insert info Area */}
 
-        {/* Show Info Table Area */}
-
-
+        <TableArea list={filteredList} />
       </C.Body>
     </C.Container>
   );
---
+++ src/components/tableArea/index.tsx
@@ -0,0 +1,30 @@
+import * as C from "./styles";
+import { Item } from "../../types/Item";
+
+type Props = {
+  list: Item[];
+};
+export const TableArea = ({ list }: Props) => {
+  return (
+    <C.Table>
+      <thead>
+        <tr>
+          <C.TableHeadColumn width={100}>Data</C.TableHeadColumn>
+          <C.TableHeadColumn width={130}>Category</C.TableHeadColumn>
+          <C.TableHeadColumn>Title</C.TableHeadColumn>
+          <C.TableHeadColumn width={150}>Value</C.TableHeadColumn>
+        </tr>
+      </thead>
+      <tbody>
+        {list.map((item, index) => (
+          <tr key={index}>
+            <td></td>
+            <td></td>
+            <td>{item.title}</td>
+            <td></td>
+          </tr>
+        ))}
+      </tbody>
+    </C.Table>
+  );
+};
