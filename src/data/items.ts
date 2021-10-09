a786ef5192dc1da005b453e5ac11c751e6795294
Icaro Peretti <icaroperetti50@gmail.com>
2021-10-09 00:25:01 -0300

    feat: styling data in table accordinf with it property

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,28 @@
-import {Item} from '../types/Item';
+import { Item } from "../types/Item";
 
 export const items: Item[] = [
-  {date:new Date(2021,9,15),category:"food",title:"McDonalds",value:32.12},
-  {date:new Date(2021,9,15),category:"food",title:"Burger King",value:28},
-  {date:new Date(2021,9,16),category:"rent",title:"Apartment rent",value:2300},
-  {date:new Date(2021,10,18),category:"salary",title:"Salary",value:4500},
+  {
+    date: new Date(2021, 9, 6),
+    category: "food",
+    title: "McDonalds",
+    value: 32.12,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "Burger King",
+    value: 28,
+  },
+  {
+    date: new Date(2021, 9, 16),
+    category: "rent",
+    title: "Apartment rent",
+    value: 2300,
+  },
+  {
+    date: new Date(2021, 10, 18),
+    category: "salary",
+    title: "Salary",
+    value: 4500,
+  },
 ];
--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -1,19 +1,33 @@
-import {Item} from '../types/Item';
+import { Item } from "../types/Item";
 
 export const getCurrentMonth = (): string => {
-    const now = new Date();
-    return `${now.getFullYear()}-${now.getMonth()+1}`;
-}
+  const now = new Date();
+  return `${now.getFullYear()}-${now.getMonth() + 1}`;
+};
 
-export const filterByMonth = (list:Item[],date:string):Item[] => {
-    let newList:Item[] = [];
-    let [year,month] = date.split('-');
+export const filterByMonth = (list: Item[], date: string): Item[] => {
+  let newList: Item[] = [];
+  let [year, month] = date.split("-");
 
-    for(let i in list){
-       if(list[i].date.getFullYear() === parseInt(year) && list[i].date.getMonth()+1 === parseInt(month)){
-           newList.push(list[i]);
-       }
+  for (let i in list) {
+    if (
+      list[i].date.getFullYear() === parseInt(year) &&
+      list[i].date.getMonth() + 1 === parseInt(month)
+    ) {
+      newList.push(list[i]);
     }
+  }
 
-    return newList;
-}
+  return newList;
+};
+
+export const formatDate = (date: Date): string => {
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+};
+
+const addZeroToDate = (num: number): string =>
+  num < 10 ? `0${num}` : `${num}`;
